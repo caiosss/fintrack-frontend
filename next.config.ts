d87124d6dfdc4ce2465ce4590f18149d838a2060
Caio <caiobarros1712@gmail.com>
2025-12-22 16:59:46 -0300

    feat: add API rewrites for backend integration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'http://191.22.105.63/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
